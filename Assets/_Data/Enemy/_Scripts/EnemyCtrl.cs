541d6811a9cdf85d2d1f45971a627a4a95ddf33a
HuyPham <phamvanhuy1709@gmail.com>
2026-08-27 11:57:36 +0700

    SaiGame C4 Ep24

--- Assets/_Data/Enemy/_Scripts/EnemyCtrl.cs
+++ Assets/_Data/Enemy/_Scripts/EnemyCtrl.cs
@@ -5,6 +5,7 @@ using UnityEngine.AI;
 
 public class EnemyCtrl : SaiMonoBehaviour
 {
+    [SerializeField] protected Transform model;
     [SerializeField] protected NavMeshAgent agent;
     public NavMeshAgent Agent => agent;
 
@@ -12,12 +13,21 @@ public class EnemyCtrl : SaiMonoBehaviour
     {
         base.LoadComponents();
         this.LoadNavMeshAgent();
+        this.LoadModel();
     }
 
     protected virtual void LoadNavMeshAgent()
     {
         if(this.agent != null) return;
         this.agent = GetComponent<NavMeshAgent>();
+        Debug.Log(transform.name + ": LoadNavMeshAgent", gameObject);
+    }
+
+    protected virtual void LoadModel()
+    {
+        if(this.model != null) return;
+        this.model = transform.Find("Model");
+        this.model.localPosition = new Vector3(0f, -0.6f, 0f);
         Debug.Log(transform.name + ": LoadNavMeshAgent", gameObject);
     }
 }
