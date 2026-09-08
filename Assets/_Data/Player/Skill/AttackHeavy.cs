f1b6c39f5efe39b8ef1b7a5572c2ab226b773304
HuyPham <phamvanhuy1709@gmail.com>
2026-09-08 17:10:15 +0700

    SaiGame C4 Ep61

---
+++ Assets/_Data/Player/Skill/AttackHeavy.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class AttackHeavy : AttackAbstract
+{
+    protected void Update()
+    {
+        this.Attacking();
+    }
+
+    protected override void Attacking()
+    {
+        // throw new System.NotImplementedException();
+    }
+}
