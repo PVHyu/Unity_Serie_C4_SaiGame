bb070969c6874f8693275085397c85f27bf80879
HuyPham <phamvanhuy1709@gmail.com>
2026-09-11 13:43:15 +0700

    SaiGame C4 Ep70

---
+++ Assets/_Data/UI/Inventory/ButtonItemInventory.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class ButtonItemInventory : ButttonAbstract
+{
+    protected override void OnClick()
+    {
+        Debug.Log("Click Item");
+    }
+}
--- Assets/_Data/UI/Inventory/InventoryUI.cs
+++ Assets/_Data/UI/Inventory/InventoryUI.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class InventoryUI : SaiSingleton<InventoryUI>
@@ -5,10 +6,25 @@ public class InventoryUI : SaiSingleton<InventoryUI>
     protected bool isShow = true;
     bool IsShow => isShow;
 
+    [SerializeField] protected ButtonItemInventory itemInventory;
+
     protected override void Start()
     {
         base.Start();
-        // this.Hide();
+        this.Hide();
+        this.HideDefaultItemInventory();
+    }
+
+    protected override void LoadComponents()
+    {
+        base.LoadComponents();
+        this.LoadButtonItemInventory();
+    }
+
+    protected virtual void LoadButtonItemInventory()
+    {
+        if(this.itemInventory != null) return;
+        this.itemInventory = GetComponentInChildren<ButtonItemInventory>();
     }
 
     public virtual void Show()
@@ -27,5 +43,10 @@ public class InventoryUI : SaiSingleton<InventoryUI>
     {
         if(this.isShow) this.Hide();
         else this.Show();
+    }
+
+    protected virtual void HideDefaultItemInventory()
+    {
+        this.itemInventory.gameObject.SetActive(false);
     }
 }
