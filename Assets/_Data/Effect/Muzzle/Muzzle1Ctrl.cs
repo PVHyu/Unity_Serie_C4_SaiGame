74292e5e1b6e3f91256441f051dd566ba771dac8
PVHyu <phamvanhuy1709@gmail.com>
2026-09-22 08:20:59 +0700

    SaiGame C4 Ep85

---
+++ Assets/_Data/Effect/Muzzle/Muzzle1Ctrl.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Muzzle1Ctrl : EffectCtrl
+{
+    public override string GetName()
+    {
+        return "Muzzle1";
+    }
+}
--- Assets/_Data/Tower/_Scripts/TowerShooting.cs
+++ Assets/_Data/Tower/_Scripts/TowerShooting.cs
@@ -3,10 +3,13 @@ using UnityEngine;
 public class TowerShooting : TowerAbstract
 {
     [SerializeField] protected int currentFirePoint = 0;
+    [SerializeField] protected float targetLoadSpeed = 1f;
     [SerializeField] protected float shootSpeed = 0.1f;
-    [SerializeField] protected float targetLoadSpeed = 1.0f;
-    [SerializeField] protected int totalKill = 0;
+    [SerializeField] protected float rotationSpeed = 4f;
+    [SerializeField] protected EnemyCtrl target;
+
     [SerializeField] protected int killCount = 0;
+    [SerializeField] protected int totalKill = 0;
     public int KillCount
     {
         get
@@ -18,33 +21,47 @@ public class TowerShooting : TowerAbstract
             killCount = value;
         }
     }
-    [SerializeField] protected float rotationSpeed = 10.0f;
-    [SerializeField] protected EnemyCtrl target;
-    // [SerializeField] protected Bullet bullet;
-     
+
+    // [SerializeField] protected SoundName shootSfxName = SoundName.LaserKickDrum;
+
+    [SerializeField] protected EffectSpawner effectSpawner;
 
     protected override void Start()
     {
         base.Start();
-        this.TargetLoading();
-        this.Shooting();
+        Invoke(nameof(this.TargetLoading), this.targetLoadSpeed);
+        Invoke(nameof(this.Shooting), this.shootSpeed);
     }
 
     protected void FixedUpdate()
     {
-        this.Looking(); 
+        this.Looking();
         this.IsTargetDead();
+    }
+
+    protected override void LoadComponents()
+    {
+        base.LoadComponents();
+        this.LoadEffectSpawner();
+    }
+
+    protected virtual void LoadEffectSpawner()
+    {
+        if (this.effectSpawner != null) return;
+        this.effectSpawner = GameObject.Find("EffectSpawner").GetComponent<EffectSpawner>();
+        Debug.Log(transform.name + ": LoadEffectSpawner", gameObject);
     }
 
     protected virtual void TargetLoading()
     {
-        Invoke(nameof(this.TargetLoading), targetLoadSpeed);
+        Invoke(nameof(this.TargetLoading), this.targetLoadSpeed);
+
         this.target = this.towerCtrl.TowerTargeting.Nearest;
     }
 
     protected virtual void Looking()
     {
-        if(this.target == null) return;
+        if (this.target == null) return;
         Vector3 directionToTarget = this.target.TowerTargetable.transform.position - this.towerCtrl.Rotator.position;
         Vector3 newDirection = Vector3.RotateTowards(
             this.towerCtrl.Rotator.forward,
@@ -63,25 +80,74 @@ public class TowerShooting : TowerAbstract
 
         FirePoint firePoint = this.GetFirePoint();
         if(firePoint == null) return;
-        Bullet newBullet = this.towerCtrl.BulletSpawner.Spawn(this.towerCtrl.Bullet, firePoint.transform.position);
-        Vector3 rotatorDirection = this.towerCtrl.Rotator.forward;
+
+        Vector3 rotatorDirection = this.towerCtrl.Rotator.transform.forward;
+
+        this.SpawnBullet(firePoint.transform.position, rotatorDirection);
+        this.SpawnMuzzle(firePoint.transform.position, rotatorDirection);
+        // this.SpawnSound(firePoint.transform.position);
+    }
+
+    protected virtual void SpawnBullet(Vector3 spawnPoint, Vector3 rotatorDirection)
+    {
+        // EffectCtrl effect = this.effectSpawner.PoolPrefabs.GetByName("Fire1");
+        // EffectCtrl newEffect = this.effectSpawner.Spawn(effect, spawnPoint);
+        // newEffect.transform.forward = rotatorDirection;
+
+        // EffectFlyAbstract effectFly = (EffectFlyAbstract)newEffect;
+        // effectFly.FlyToTarget.SetTarget(this.target.TowerTargetable.transform);
+
+        // Debug.Log("Bullet's position: " + newEffect.transform.position, gameObject);
+
+        // newEffect.gameObject.SetActive(true);
+        Bullet newBullet = this.towerCtrl.BulletSpawner.Spawn(this.towerCtrl.Bullet, spawnPoint);
         newBullet.transform.forward = rotatorDirection;
         newBullet.gameObject.SetActive(true);
     }
 
+    protected virtual void SpawnMuzzle(Vector3 spawnPoint, Vector3 rotatorDirection)
+    {
+        EffectCtrl effect = this.effectSpawner.PoolPrefabs.GetByName("Muzzle1");
+        EffectCtrl newEffect = this.effectSpawner.Spawn(effect, spawnPoint);
+        newEffect.transform.forward = rotatorDirection;
+        newEffect.gameObject.SetActive(true);
+    }
+
     protected virtual FirePoint GetFirePoint()
     {
-        FirePoint firePoint = this.towerCtrl.FirePoints[currentFirePoint];
-        
+        FirePoint firePoint = this.towerCtrl.FirePoints[this.currentFirePoint];
+        // this.currentFirePoint++;
+        // if (this.currentFirePoint == this.towerCtrl.FirePoints.Count) this.currentFirePoint = 0;
         return firePoint;
     }
 
     protected virtual bool IsTargetDead()
     {
-        if(this.target == null || !this.target.EnemyDamageReceiver.IsDead()) return false;
-        this.KillCount++;
+        if (this.target == null) return true;
+        if (!this.target.EnemyDamageReceiver.IsDead()) return false;
+        this.killCount++;
         this.totalKill++;
         this.target = null;
         return true;
     }
+
+    public virtual bool DeductKillCount(int count)
+    {
+        if (this.killCount < count) return false;
+        this.killCount -= count;
+        return true;
+    }
+
+    // protected virtual void SpawnSound(Vector3 position)
+    // {
+    //     SFXCtrl newSfx = SoundManager.Instance.CreateSfx(this.shootSfxName);
+    //     newSfx.transform.position = position;
+    //     newSfx.gameObject.SetActive(true);
+    // }
 }
+
+
+
+
+
+
